6fa9c8e92e57094fa60d299439a4dcca2e9de8e6
Christopher Paskewich <c.paskewich@centre.edu>
2019-10-10 10:56:45 -0400

    Got basics into each file

--- studio.js
+++ studio.js
@@ -1,1 +1,9 @@
+// https://ghibliapi.herokuapp.com/
+var studioPromise = d3.json("https://ghibliapi.herokuapp.com/films")
 
+studioPromise.then(
+    function(film)
+    {
+        d3.select("#banner").text("select")
+    }
+)
