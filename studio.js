15e8038fe71fc45beb46365b8cdaaa33eb5aca42
jcpask <c.paskewich@centre.edu>
2019-10-14 23:52:10 -0400

    further work from home

--- studio.js
+++ studio.js
@@ -1,9 +1,33 @@
 // https://ghibliapi.herokuapp.com/
-var studioPromise = d3.json("https://ghibliapi.herokuapp.com/films")
+var studioPromise = d3.html("https://ghibliapi.herokuapp.com/films")
 
 studioPromise.then(
-    function(film)
+    function(data)
+        {
+            filmTitle(data)
+        })
+
+studioPromise.then(
+    function(films)
     {
-        d3.select("#banner").text("Click on a film you would like to know more about")
+        d3.select("#banner").text("Click on a film you would like to know more about");
+    }
+    function(err)
+    {
+        d3.select("#banner").text("No films now");
     }
 )
+
+var makeFilmList=function(films)
+    {
+     var pDiv=d3.select("#listStart").append("ol")
+    .selectAll("li")
+    .data(films)
+    .enter()
+    .append("li")
+    .text(function(film)
+         {
+        var filmTitle = return film.title
+    })
+        }
+
